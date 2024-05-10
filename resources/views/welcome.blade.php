123e050b02082404418e7d20d5d987a832e44727
rcravens <bob.cravens@gmail.com>
2024-05-10 16:40:43 -0500

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -910,7 +910,7 @@
                                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
                                         <th scope="col" class="px-6 py-3">
-                                            Place A
+                                            Place
                                         </th>
                                         <th scope="col" class="px-6 py-3">
                                             Distance
