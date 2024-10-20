48fce28e9170f279956f042e164ec04d30af982b
rcravens <bob.cravens@gmail.com>
2024-10-20 12:06:29 -0500

    added TestJob to schedule

--- routes/console.php
+++ routes/console.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Jobs\InspireJob;
+use App\Jobs\TestJob;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::job( new InspireJob() )
+        ->everyMinute()
+        ->onOneServer();
+
+Schedule::job(new TestJob() )
         ->everyMinute()
         ->onOneServer();
 
